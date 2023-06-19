aa56df8b8a49f32c821405cc86e052d73c8c6233
LanaVol <svolosh4yk@gmail.com>
2023-06-19 21:32:18 +0300

    update with all components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import "./App.css";
 import { Header } from "./layout/Header";
 import { Footer } from "./layout/Footer";
+import { Main } from "./layout/Main";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    movies: [],
+    error: "",
+  };
+
+  componentDidMount() {
+    fetch("http://www.omdbapi.com?apikey=f10742f1&s=matrix")
+      .then((res) => res.json())
+      .then((data) => this.setState({ movies: data.Search }))
+      .catch((err) => {
+        console.log("hjhjhg");
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+
+        {this.state.error ? (
+          <p>Oops, not found this page</p>
+        ) : (
+          <Main movies={this.state.movies} />
+        )}
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
