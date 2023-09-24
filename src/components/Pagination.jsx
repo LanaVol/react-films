6408156c0ef8196d242c09d2aef500a04fd81400
LanaVol <svolosh4yk@gmail.com>
2023-09-24 23:14:07 +0300

    update pagination component

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,6 +4,7 @@ class PaginationComponent extends React.Component {
   state = {
     title: "",
     pageNumber: 1,
+    countPaginPerPage: 7,
   };
 
   handleChoosePage = (e) => {
@@ -18,8 +19,6 @@ class PaginationComponent extends React.Component {
       this.setState({ pageNumber: this.state.pageNumber + 1 });
       this.props.setPages(this.state.pageNumber);
     }
-    // console.log(this.props.allPages.length);
-    // console.log(this.state.pageNumber);
   };
   previousPage = () => {
     if (+this.state.pageNumber === 1) {
@@ -30,23 +29,54 @@ class PaginationComponent extends React.Component {
     }
   };
 
+  showPaginationPagePerPage = () => {
+    let firstPaginNumber = lastPaginNumber - this.state.countPaginPerPage;
+    let lastPaginNumber = this.state.pageNumber * this.state.countPaginPerPage;
+    let currentPaginPages = this.props.allPages.slice(
+      firstPaginNumber,
+      lastPaginNumber
+    );
+    const listPaginNumber = [];
+
+    for (
+      let i = 1;
+      i <= Math.ceil(this.props.allPages / this.state.countPaginPerPage);
+      i++
+    ) {
+      listPaginNumber.push(i);
+    }
+  };
+
   render() {
+    const { pageNumber } = this.state;
+    const { allPages } = this.props;
     return (
       <ul className="pagination">
         <li
-          className={+this.state.pageNumber === 1 ? "disabled" : "waves-effect"}
+          className={+pageNumber === 1 ? "disabled" : "waves-effect"}
           onClick={this.previousPage}
+          disabled={pageNumber === 1}
         >
           <a href="#!">
             <i className="material-icons">chevron_left</i>
           </a>
         </li>
-        {this.props.allPages.map((number, i) => (
+
+        <li
+          onClick={this.handleChoosePage}
+          className={
+            +pageNumber === allPages[0]
+              ? "active cyan darken-4"
+              : "waves-effect"
+          }
+        >
+          <a href="#!">{allPages[0]}</a>
+        </li>
+
+        {allPages.map((number, i) => (
           <li
             className={
-              +this.state.pageNumber === number
-                ? "active cyan darken-4"
-                : "waves-effect"
+              +pageNumber === number ? "active cyan darken-4" : "waves-effect"
             }
             key={number}
             onClick={this.handleChoosePage}
@@ -54,14 +84,26 @@ class PaginationComponent extends React.Component {
             <a href="#!">{number}</a>
           </li>
         ))}
+
+        <li
+          onClick={this.handleChoosePage}
+          className={
+            +pageNumber === allPages[allPages.length - 1]
+              ? "active cyan darken-4"
+              : "waves-effect"
+          }
+        >
+          <a href="#!">{allPages[allPages.length - 1]}</a>
+        </li>
+
         <li
           className={
-            +this.state.pageNumber ===
-            this.props.allPages[this.props.allPages.length - 1]
+            +pageNumber === allPages[allPages.length - 1]
               ? "disabled"
               : "waves-effect"
           }
           onClick={this.nextPage}
+          disabled={!allPages.length}
         >
           <a href="#!">
             <i className="material-icons">chevron_right</i>
